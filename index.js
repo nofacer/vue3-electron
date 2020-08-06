52fc8b8d623b6b31d751156b3cbbdc3f598ab7dc
九日木彡 <nofacer@outlook.com>
2020-08-06 15:29:24 +0800

    use webpack to pack js files

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+import _ from 'lodash';
+
 function component() {
     const element = document.createElement('div');
-    // Lodash, currently included via a script, is required for this line to work
+    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     return element;
   }
-  
 document.body.appendChild(component());
